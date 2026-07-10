1adc69091f125b9fbb5ec7c52204d924a9b0fdae
Debi Thomas <legacygoldlifeline@protonmail.com>
2026-07-10 00:03:38 -0400

    Added stellar-wallets-kit packages to optimizeDeps in vite.config.js

--- HMP/EH2/EH2_telegram_mini_app/vite.config.js
+++ HMP/EH2/EH2_telegram_mini_app/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  optimizeDeps: {
+  include: [
+  '@creit-tech/stellar-wallets-kit',
+  '@creit-tech/stellar-wallets-kit/modules/utils',
+  '@creit-tech/stellar-wallets-kit/modules/wallet-connect',
+  '@creit-tech/stellar-wallets-kit/types',// <-- the JSR packages names
+  ],
+  },
   base: '/empower_humanity2/',   // VERY IMPORTANT
   plugins: [require('lit-plugin-html')] // Ensures proper template compilation
 })
